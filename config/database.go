88d722ab99d7cac58d725a8dfa6d070eb2ba4f7d
damarbagas-234 <muhammaddamar107@gmail.com>
2026-06-28 16:20:45 +0700

    memperbarui auto migrate untuk model cart dan order

--- config/database.go
+++ config/database.go
@@ -20,22 +20,26 @@ func InitDatabase() {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+
 	// Format DSN (Data Source Name) untuk MySQL
 	// Format: user:pass@tcp(host:port)/dbname?params
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, dbname,
 	)
+
 	// Konfigurasi GORM
 	gormConfig := &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info), // Log semua query SQL
 	}
+
 	// Buka koneksi
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), gormConfig)
 	if err != nil {
 		log.Fatalf("Gagal koneksi ke database: %v", err)
 	}
+
 	// Setup connection pool
 	sqlDB, err := DB.DB()
 	if err != nil {
@@ -43,14 +47,19 @@ func InitDatabase() {
 	}
 	sqlDB.SetMaxOpenConns(25) // Maksimal 25 koneksi terbuka
 	sqlDB.SetMaxIdleConns(10) // Maksimal 10 koneksi idle
+
 	// AutoMigrate: buat/update tabel sesuai struct model
 	// GORM akan buat tabel jika belum ada
 	err = DB.AutoMigrate(
 		&models.User{},
 		&models.Product{},
+		&models.CartItem{},
+		&models.Order{},
+		&models.OrderItem{},
 	)
 	if err != nil {
 		log.Fatalf("AutoMigrate gagal: %v", err)
 	}
+
 	log.Println("Database terhubung dan tabel sudah di-migrate")
 }
